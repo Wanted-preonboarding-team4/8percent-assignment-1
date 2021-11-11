04b335e5085a7f25e903f785b4b1c5afbc01b47e
Daehoon <foreat_14@naver.com>
2021-11-12 04:07:53 +0900

    feat : withdraw 라우팅 추가

--- account/urls.py
+++ account/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
-from account.views import AccountView, DepositView
+from account.views import AccountView, DepositView, WithdrawView
 
 urlpatterns = [
     path('', AccountView.as_view()),
-    path('/deposit', DepositView.as_view())
+    path('/deposit', DepositView.as_view()),
+    path('/withdraw', WithdrawView.as_view())
 ]
