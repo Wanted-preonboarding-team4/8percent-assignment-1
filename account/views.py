ab6f08364aae208bfb3ab3780ff6cc815d7e9ba2
Daehoon <foreat_14@naver.com>
2021-11-12 02:31:19 +0900

    feat : 입금 뷰 생성 - 사용자 정보, 입금 금액을 받아 db에 저장하는 로직

--- account/views.py
+++ account/views.py
@@ -1,3 +1,31 @@
-from django.shortcuts import render
+import json, re, bcrypt, jwt
 
-# Create your views here.
+from django.http import JsonResponse
+from django.views import View
+from account.models import Account, Transaction
+from users.utils import login_decorator
+
+
+class DepositView(View):
+    # @login_decorator
+    def post(self, request):
+        try:
+            data = json.loads(request.body)
+            account = Account.objects.get(account_number=data['account_number'])
+
+            if not account:
+                return JsonResponse({'message': '일치하는 계좌가 없습니다.'}, status=404)
+
+            if account.user.email != data['user']['email']:
+                return JsonResponse({'message': '본인 계좌가 아닙니다.'}, status=404)
+
+            if not bcrypt.checkpw(data['password'].encode('utf-8'), account.password.encode('utf-8')):
+                return JsonResponse({'message': '비밀번호가 틀렸습니다.'}, status=404)
+
+            # if account.user.password != data['user']['password']:
+            #     return JsonResponse({'message': '비밀번호가 틀렸습니다.'}, status=404)
+            Account.objects.filter(account_number=data['account']).update(balance=account.balance + int(data['account_number']))
+            return JsonResponse({'message': '입금 성공'}, status=200)
+
+        except KeyError:
+            return JsonResponse({'message':'KEY_ERROR'},status=400)
